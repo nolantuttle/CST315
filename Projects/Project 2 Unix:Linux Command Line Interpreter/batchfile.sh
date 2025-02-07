0099aeea3957c3996d084b8f6878507743e30f8f
Nolan Tuttle <NTuttle1@my.gcu.edu>
2025-02-07 12:29:44 -0700

    Modified string input, semicolon separation working, batch file partially working

---
+++ Projects/Project 2 Unix:Linux Command Line Interpreter/batchfile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ls -l
+pwd
+ps
+whoami
--- Projects/Project 2 Unix:Linux Command Line Interpreter/shellSimulator.c
+++ Projects/Project 2 Unix:Linux Command Line Interpreter/shellSimulator.c
@@ -6,60 +6,84 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-// function to concatenate two strings together with a space in the middle
-char *concat(const char *s1, const char *s2){
-    char *result = malloc(strlen(s1) + strlen(s2) + 2); // add 2 so there is room
-    // for the null terminator character and space
-    strcpy(result, s1);
-    strcat(result, " ");
-    strcat(result, s2);
-    return result;
-}
-
 // we have to use execv() and fork() to start a child process 
 // we need support for multiple simultaneous commands separated by semicolons
 // ^ this means use wait() and waitpid()
 
-// mannnnnnnnnnnnnn
+// argc is the argument count, if they give 1 then a batch file is provided
+int main(int argc, char *argv[]){
 
-int main(){
-    char str[100]; // allocate space for input string
-    char *word1;   // first word before whitespace
-    char *word2;   // second word (Everything after first word)
-    pid_t pid;  // child process variable
-
-    while(1)
+    if(argc == 2)   // batch file
     {
+        // Get the filename (path to the batch file)
+        char *batch_file = argv[1];
 
-        fgets(str, sizeof(str), stdin); // Read input, including spaces
-        str[strcspn(str, "\n")] = 0;    // Remove newline character
+        // Open the batch file for reading
+        FILE *file = fopen(batch_file, "r");
+        char str[100];
 
-        word1 = strtok(str, " "); // fetches first word, space delimiter
-        word2 = strtok(NULL, ""); // fetches second word, no delimiter
+        while (fgets(str, sizeof(str), file)) {
+            str[strcspn(str, "\n")] = 0;    // Remove newline character
 
-        // if there is no second word, initializes it to an empty string
-        if (word2 == NULL){
-            word2 = "";
-        }
-
-        //system(concat(word1, word2)); // concatenates the two words
-
-        char* args[] = {"/bin/bash", "-c", concat(word1, word2), NULL};
-
-        pid = fork(); // create a child process
-
-        if(pid == 0){
-            if (execv("/bin/bash",args) == -1)
-            {
-                perror("exec failed");
-                return 1;
+            // Skip empty lines and comments
+            if (str[0] == '\0' || str[0] == '#') {
+                continue;
             }
-            exit(1);
+
+            char commandECHO[100];
+            strcpy(commandECHO, "echo ");
+            strcat(commandECHO, str);
+
+            char* args[] = {"/bin/bash", "-c", commandECHO, NULL};  // Runs current command using echo
+            execv("/bin/bash",args);    // Executes current command
+
+            char* args2[] = {"/bin/bash", "-c", str, NULL};  // Actually runs the command
+            execv("/bin/bash",args2);  // Executes current command  
         }
-        else if (pid >0){
-            wait(NULL);
+
+        fclose(file);
+    }
+    else    // no batch file
+    {
+        char str[100]; // allocate space for input string
+        while(1){
+            fgets(str, sizeof(str), stdin); // Read input, including spaces
+            str[strcspn(str, "\n")] = 0;    // Remove newline character
+
+            // splits up the input into semicolon-separated tokens
+            char* command = strtok(str, ";"); 
+
+            while(command != NULL)
+            {
+
+                char* args[] = {"/bin/bash", "-c", command, NULL};
+
+                pid_t pid = fork(); // create a child process
+
+                if(pid == 0){
+                    if (execv("/bin/bash",args) == -1)
+                    {
+                        perror("exec failed");
+                        exit(1);
+                    }
+                }
+                else if (pid > 0){
+
+                    int status;
+                    pid_t childPID = waitpid(pid, &status, 0);
+                }
+                command = strtok(NULL, ";");
+                
+            }
+
         }
     }
+    // Get the filename (path to the batch file)
+    char *batch_file = argv[1];
+
+    // Open the batch file for reading
+    FILE *file = fopen(batch_file, "r");
+    
 
     return 0;
 }
