30acf0622dd57288215f915af71a12c4109b3f12
Nolan Tuttle <NTuttle1@my.gcu.edu>
2025-02-16 19:38:38 -0700

    Finished Assignment 2

--- Projects/Assignment 2 Monitors and Semaphores/monitors.C
+++ Projects/Assignment 2 Monitors and Semaphores/monitors.C
@@ -1,11 +1,14 @@
 /*Written by Mathew Hobson*/
-#include<stdio.h>
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 typedef struct Monitor
 {
-
     int readers; //Number of active readers
     int writing; //Number of active writers
 }Monitor;
@@ -23,7 +26,7 @@ void startRead(Monitor *m){
         printf("[Reader BLOCKED] A writer is writing\n");
     }
     m->readers++;
-    printf("[Reader Start], %d readers started reading\n",m->readers);
+    printf("[Reader Start], %d readers are reading\n",m->readers);
 }
 void endRead(Monitor *m){
     if(m->readers==0)return;
@@ -48,25 +51,58 @@ void endWrite(Monitor *m) {
     printf("[Writer END] Writing finished. Readers/Writers can proceed.\n");
 }
 
-// Monitor Cleanup (Optional)
-void destroyMonitor(Monitor *m) {
-    // No resources to free, but placeholder for future extensions
-}
-
 int main() {
-    Monitor monitor;
-    initMonitor(&monitor);
 
-    // Simulating function calls in a sequential execution
-    startRead(&monitor);
-    startRead(&monitor);
-    endRead(&monitor);
-    startWrite(&monitor);
-    startRead(&monitor);  // This should be blocked
-    endWrite(&monitor);
-    startRead(&monitor);
-    endRead(&monitor);
-    destroyMonitor(&monitor);
+    // Creating shared memory to initialize the monitor
+    int memID = shmget(IPC_PRIVATE, sizeof(Monitor), IPC_CREAT | 0666);
+    Monitor *monitor = (Monitor *)shmat(memID, NULL, 0);
+    initMonitor(monitor);
 
+    int totalReaders = 5;
+    int totalWriters = 3;
+
+    // Forking reader processes
+    for (int i = 0; i < totalReaders; i++) {
+        pid_t pid = fork();
+        if (pid == 0) { // Child process (reader)
+            startRead(monitor);  // Reader starts
+            sleep(2);            // Simulate reading
+            endRead(monitor);    // Reader ends
+            exit(0);             // Exit child process
+        }
+    }
+
+    // Forking writer processes (these writers will be blocked)
+    for (int i = 0; i < totalWriters; i++) {
+        pid_t pid = fork();
+        if (pid == 0) { // Child process (writer)
+            startWrite(monitor);  // Writer starts
+            sleep(3);             // Simulate writing
+            endWrite(monitor);    // Writer ends
+            exit(0);              // Exit child process
+        }
+    }
+
+    // Wait for readers to be done
+    sleep(5);
+
+    // Forking writer processes (this writer will not be blocked)
+    pid_t pid = fork();
+    if (pid == 0) { // Child process (writer)
+            startWrite(monitor);  // Writer starts
+            sleep(1);             // Simulate writing
+            endWrite(monitor);    // Writer ends
+            exit(0);              // Exit child process
+        }
+
+    // Wait for all child processes to finish
+    for (int i = 0; i < 5 + 3; i++) {
+        wait(NULL);
+    }
+
+    // Clean up shared memory
+    shmdt(monitor);
+    shmctl(memID, IPC_RMID, NULL);
+    
     return 0;
 }
--- Projects/Assignment 2 Monitors and Semaphores/semaphores.C
+++ Projects/Assignment 2 Monitors and Semaphores/semaphores.C
@@ -1,117 +1,141 @@
-#include<stdio.h>
-#include<pthread.h>
-#include<semaphore.h>
-#include<unistd.h>
+/*Written by Nolan Tuttle*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <fcntl.h>
 
 // Declaring semaphores
-sem_t mutex;
-sem_t writerSemaphore;
-sem_t readerSemaphore;
+sem_t *mutex;
+sem_t *writerSemaphore;
+sem_t *readerSemaphore;
 
 // This is the shared resource being read/written to
-int resource = 0;
+int *resource = 0;
 
-int activeReaders = 0;
+// Number of active readers
+int *activeReaders = 0;
 
-void *reader(void *args)
+void reader(int args)
 {
     // Current semaphore will wait until the resource is unlocked
-    sem_wait(&mutex);
+    sem_wait(mutex);
 
-    activeReaders++;
-    if (activeReaders == 1) {
+    (*activeReaders)++;
+    if (*activeReaders == 1) {
         // If this is the first reader, wait for the writer
-        sem_wait(&writerSemaphore);
+        sem_wait(writerSemaphore);
     }
 
     // Signals the mutex to unlock
-    sem_post(&mutex);
+    sem_post(mutex);
 
     // Simulate reading the shared resource
-    printf("Reader %ld is reading. Shared resource: %d\n", pthread_self(), resource);
+    printf("Reader %d is reading. Shared resource value: %d\n", args, *resource);
 
     // Waits to simulate the read execution
     sleep(2);
 
     // Waits for mutex before removing a reader
-    sem_wait(&mutex);
+    sem_wait(mutex);
 
-    activeReaders--;
+    (*activeReaders)--;
     // If there are no active readers, signal the writer semaphore
-    if (activeReaders == 0) {
-        sem_post(&writerSemaphore);
+    if (*activeReaders == 0) {
+        sem_post(writerSemaphore);
     }
 
     // Unlocks the mutex (binary semaphore)
-    sem_post(&mutex);
-
-    return NULL;
+    sem_post(mutex);
 }
 
-void *writer(void *args)
+void writer(int args)
 {
-    // Sleep so that console output looks better
-    // Since everything runs concurrently, the output is different each time without this delay
-    sleep(2);
     
     // Writer semaphore will wait for signal
-    sem_wait(&writerSemaphore);
+    sem_wait(writerSemaphore);
 
     // Write to shared resource
-    resource++;
+    (*resource)++;
 
     // Print statement to show resource being updated
-    printf("Writer %ld is writing\n", pthread_self());
-    printf("Done writing value %d\n", resource);
+    printf("Writer %d is writing\n", args);
+    printf("Done writing value %d\n", *resource);
 
     // Waits to simulate write execution
     sleep(2);
 
     // Signals that the semaphore is done writing
-    sem_post(&writerSemaphore);
-
-    return NULL;
+    sem_post(writerSemaphore);
 }
 
 int main()
 {    
-    int totalReaders = 5;
-    int totalWriters = 3;
+    int totalReaders = 3;
+    int totalWriters = 2;
 
     pthread_t readers[totalReaders];
     pthread_t writers[totalWriters];
 
+    // Creating a shared piece of memory
+    // Sets the key, block size (2*sizeof(int)), and flag for shared memory.
+    int memID = shmget(IPC_PRIVATE, 2 * sizeof(int), IPC_CREAT | 0666);
+
+    // Attaches shared memory to process
+    resource = (int *)shmat(memID, NULL, 0);
+
+    // This should be right after resource in shared memory
+    activeReaders = resource + 1;
+
     // Initializing semaphores
-    sem_init(&mutex, 0, 1);            // Mutex initialized to 1 (binary semaphore)
-    sem_init(&writerSemaphore, 0, 1);   // Write semaphore to allow only one writer
-    sem_init(&readerSemaphore, 0, 1);    // Semaphore to control access for readers
+    mutex = sem_open("/mutex", O_CREAT, 0666, 1);  // Mutex semaphore
+    writerSemaphore = sem_open("/writerSemaphore", O_CREAT, 0666, 1);  // Writer semaphore
+    readerSemaphore = sem_open("/readerSemaphore", O_CREAT, 0666, 1);  // Reader semaphore
 
-    // Creates 5 reader threads
-    for(int i = 0; i < totalReaders; i++)
-    {
-        pthread_create(&readers[i], NULL, reader, NULL);
+    // Forking a single reader process
+    pid_t pid = fork();
+        if (pid == 0) {
+            // Reader process
+            reader(1);  // Passes in process number for print stuff
+            exit(0);  // Terminate child process after it finishes reading
+        }
+
+    // Forking the writer processes
+    for (int i = 0; i < totalWriters; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Writer process
+            writer(i);  // Passes in process number for print stuff
+            exit(0);  // Terminate child process after it finishes writing
+        }
     }
 
-    // Creates 5 writer threads
-    for(int i = 0; i < totalWriters; i++)
-    {
-        pthread_create(&writers[i], NULL, writer, NULL);
+    // Forking the reader processes
+    for (int i = 0; i < totalReaders; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Reader process
+            reader(i);  // Passes in process number for print stuff
+            exit(0);  // Terminate child process after it finishes reading
+        }
     }
 
-    // Makes sure we wait for all threads to finish
-    for(int i = 0; i < totalReaders; i++)
-    {
-        pthread_join(readers[i], NULL);
-    }
-    for(int i = 0; i < totalWriters; i++)
-    {
-        pthread_join(writers[i], NULL);
+    // Wait for all child processes to be done
+    for (int i = 0; i < totalReaders + totalWriters; i++) {
+        wait(NULL);
     }
 
     // Destroy allocated resources
-    sem_destroy(&mutex);
-    sem_destroy(&writerSemaphore);
-    sem_destroy(&readerSemaphore);
+    sem_close(mutex);
+    sem_close(writerSemaphore);
+    sem_close(readerSemaphore);
 
-return 0;
+    // Cleanup of shared memory
+    shmdt(resource);
+    shmctl(memID, IPC_RMID, NULL);
+
+    return 0;
 }
