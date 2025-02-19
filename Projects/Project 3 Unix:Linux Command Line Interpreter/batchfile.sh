ed19aee4b60a5ac20515acf0002d13f191a5ace2
Nolan Tuttle <NTuttle1@my.gcu.edu>
2025-02-19 11:08:27 -0700

    Initial commit project 3

---
+++ Projects/Project 3 Unix:Linux Command Line Interpreter/batchfile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "This is the first line of the batch file!"
+echo "This is the second line!"
+echo "This program runs using parent and child processes."
+echo "For all commands given separated by a semicolon (;)"
+echo "they will be executed concurrently."
+echo ""
