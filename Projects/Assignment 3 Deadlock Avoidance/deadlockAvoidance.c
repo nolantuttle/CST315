18651c5fd11fc20db9b37da7ea83f30b10258af6
MathewHobson03 <amandacody06@gmail.com>
2025-02-28 10:32:50 -0700

    YEAHHHHHHHHHH Co-authored-by: nolantuttle <nolantuttle@users.noreply.github.com>

--- Projects/Assignment 3 Deadlock Avoidance/deadlockAvoidance.c
+++ Projects/Assignment 3 Deadlock Avoidance/deadlockAvoidance.c
@@ -1,137 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <semaphore.h>
-#include <fcntl.h> // For O_CREAT flag
-#include <sys/types.h>
-#include <unistd.h> // For fork()
-#include <sys/wait.h>
-#include <signal.h>
+#include <pthread.h>
 #include <time.h>
+#include <errno.h>
 
-sem_t *resource_semaphore;
-#define NUM_PROCESSES 5
-#define RESOURCE_TIMEOUT 8
-time_t bruh[NUM_PROCESSES];
 
-pid_t pids[NUM_PROCESSES];
+sem_t resource_semaphore; // semefore
+int shared_resource;
+#define RT 4
 
-void resource1(void *arg)
+
+void* process1(void* args)
 {
-    sem_wait(resource_semaphore);
-    printf("Resource entered...\n");
-    sleep(3);
-    printf("Exiting...\n");
-    sem_post(resource_semaphore);
-}
-void resource2(void *arg)
-{
-    sem_wait(resource_semaphore);
-    printf("Resource entered...\n");
-    sleep(3);
-    printf("Exiting...\n");
-    sem_post(resource_semaphore);
-}
-void restart_process(int index)
-{
-    printf("Process %d is being restarted...\n", pids[index]);
+    struct timespec ts; 
+    clock_gettime(CLOCK_REALTIME, &ts);
+    ts.tv_sec += RT;
 
-    // Kill the starved process
-    kill(pids[index], SIGKILL);
-    waitpid(pids[index], NULL, 0); // Wait for termination
 
-    // Fork a new process to replace the killed one
-    pids[index] = fork();
-    if (pids[index] < 0)
-    {
-        perror("Fork failed");
-        exit(1);
+    if(sem_timedwait(&resource_semaphore, &ts) == 0){
+        printf("Resource entered by thead %i.\n", (int)pthread_self());
+        sleep(3);
+        printf("Thread %d released resource.\n", (int)pthread_self());
+        sem_post(&resource_semaphore);
+        shared_resource = rand();
     }
-    else if (pids[index] == 0)
-    { // Child process
-        printf("New Process %d created to replace starved process.\n", getpid());
-        if (index % 2 == 0)
-        {
-            resource1(NULL);
+    else{
+        if(errno == ETIMEDOUT){
+            printf("Thread %d starved (timeout reached). Restarting...\n", (int)pthread_self());
         }
-        else
-        {
-            resource2(NULL);
-        }
-        exit(0);
-    }
-}
-
-// Check for starvation periodically
-void starvation_check(int sig)
-{
-    printf("\n[Starvation Check] Checking for starved processes...\n");
-
-    for (int i = 0; i < NUM_PROCESSES; i++)
-    {
-        if (time(NULL) - bruh[i] > RESOURCE_TIMEOUT)
-        {
-            restart_process(i);
+        else{
+            perror("sem_timedwait failed");
         }
     }
-
-    alarm(RESOURCE_TIMEOUT); // Reset starvation timer
 }
-
-int main()
-{
-    // Initialize semaphore
-
-    resource_semaphore = sem_open("/resource_semaphore", O_CREAT, 0644, 1);
-    if (resource_semaphore == SEM_FAILED)
+   
+    // mcdaondlsds
+int main() //mane funktion
     {
-        perror("Semaphore initialization failed");
-        exit(1);
+
+        sem_init(&resource_semaphore, 0, 1);
+    
+        // pthread ids
+        pthread_t id1, id2;
+
+        pthread_create(&id1, NULL, process1, NULL);
+        
+        pthread_create(&id2, NULL, process1, NULL);
+
+        pthread_join(id1, NULL);
+
+        pthread_join(id2, NULL);
+
+        
+        
     }
-
-    printf("Semaphore initialized. Resource is available.\n");
-
-    // Set up starvation check using SIGALRM
-    signal(SIGALRM, starvation_check);
-    alarm(RESOURCE_TIMEOUT); // Start first starvation timer
-
-    // Create multiple processes
-    for (int i = 0; i < NUM_PROCESSES; i++)
-    {
-        pids[i] = fork();
-
-        if (pids[i] < 0)
-        {
-            perror("Fork failed");
-            exit(1);
-        }
-        else if (pids[i] == 0)
-        { // Child process
-            printf("Process %d created.\n", getpid());
-            if (i % 2 == 0)
-            {
-                bruh[i] = time(NULL);
-
-                resource1(NULL);
-            }
-            else
-            {
-                bruh[i] = time(NULL);
-
-                resource2(NULL);
-            }
-            exit(0);
-        }
-    }
-
-    // Parent waits for processes but also checks for starvation
-    while (1)
-    {
-        wait(NULL);
-    }
-
-    // Cleanup semaphore
-    sem_close(resource_semaphore);
-    sem_unlink("/resource_semaphore");
-
-    return 0;
-}
+    
+    
